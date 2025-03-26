b55ebcd8d692679912e247482c719aac91a0ad87
Starboy <samskywalker001@gmail.com>
2025-03-26 05:55:32 +0000

    initial ui

---
+++ src/app/navigation-menubar.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import Link from "next/link";
+
+export default function NavigationMenuBar() {
+  return (
+    <div className="flex h-16 bg-gradient-to-b from-blue-950 via-blue-600 to-blue-300">
+      <div className="m-4 flex size-9 w-16 flex-auto">
+        <Link href="/">
+          <img src="/myspace-logo.svg" alt="Icon" className="w-14"/>
+        </Link>
+      </div>
+      <div className="flex size-9 flex-none md:flex-auto"></div>
+      <div className="flex size-12 flex-grow flex-row-reverse gap-4 p-4 tracking-tighter md:tracking-normal">
+        <Link href="/about">About</Link>
+        <Link href="/about">Profile</Link>
+        <Link href="/blog">Blog</Link>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/ui/footer.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import NextLogo from "~/components/ui/next-logo";
+import { ModeToggle } from "~/components/theme-toggle";
+
+export default function Footer() {
+  return (
+    <div className="bg-gradient-to-b from-blue-950 via-blue-600 to-blue-300">
+      <div className="flex flex-row items-center justify-center justify-items-end p-3 text-xs text-gray-600">
+        <div className="mr-2">
+          <ModeToggle></ModeToggle>
+        </div>
+        <footer>
+          <div>
+            <p>Built w/</p>
+            <div className="w-36">
+              <NextLogo></NextLogo>
+            </div>
+            <p>
+              &copy; 12025,{" "}
+              <a
+                href="https://github.com/skywalkerSam/"
+                className="hover:text-sky-400 hover:underline focus:text-sky-400"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                @skywalkerSam
+              </a>
+            </p>
+          </div>
+        </footer>
+      </div>
+    </div>
+  );
+}
