a918a33c884ec34c90938643523f9f395a75a9ec
Starboy <samskywalker001@gmail.com>
2025-03-26 19:30:32 +0000

    myspace logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,25 @@ import Link from "next/link";
 export default function HomePage() {
   return (
     <div>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-blue-300 via-blue-600 to-blue-950 text-white">
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16"></div>
+      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-blue-300 via-blue-600 to-blue-950 text-sky-400">
+        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
+          <div>
+            <Link
+              href="https://myspace.com"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              <div className="flex">
+                <img
+                  src="/myspace-logo.svg"
+                  alt="Icon"
+                  className="p-1 w-20 md:w-24"
+                />
+                <p className="p-1 text-4xl md:text-5xl">myspace</p>
+              </div>
+            </Link>
+          </div>
+        </div>
       </main>
     </div>
   );
