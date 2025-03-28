506aa5bd639a5a16be529d6d7566fd5994f4530e
Starboy <samskywalker001@gmail.com>
2025-03-28 22:13:35 +0000

    hotfix

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -30,7 +30,7 @@ interface Props {
  * @see https://nextjs.org/docs/app/api-reference/functions/next-response
  */
 export default async function BlogPage({ params }: Props) {
-  const { slug } = await params;
+  const { slug } = params;
   const posts: Post[] | undefined = await getPosts();
   // console.log(posts);
   const post = posts?.find((post) => post?.slug === slug);
@@ -48,7 +48,7 @@ export default async function BlogPage({ params }: Props) {
   return (
     <div className="text-sky-400">
       <div className="bg-gradient-to-b from-blue-300 via-blue-500 to-blue-600">
-        <h1 className="flex items-center justify-center text-4xl text-gray-950 hover:text-blue-950 md:text-5xl p-10">
+        <h1 className="flex items-center justify-center p-10 text-4xl text-gray-950 hover:text-blue-950 md:text-5xl">
           {post?.title}
         </h1>
       </div>
